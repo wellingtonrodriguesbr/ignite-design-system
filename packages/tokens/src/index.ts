ecbe41f1d543742f6e20494503c26a81269d9854
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-10-10 06:44:38 -0300

    initial commit

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./colors";
+export * from "./fonts";
+export * from "./font-sizes";
+export * from "./font-weights";
+export * from "./line-heights";
+export * from "./radii";
+export * from "./space";
