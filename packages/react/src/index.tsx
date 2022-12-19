aacadc234874cbf9b40410d012b2172b8bac6f00
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-12-19 05:49:21 -0300

    chore: fix imports

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,4 +1,4 @@
-import { colors } from '@ignite-ui/tokens'
+import { colors } from '@ignite-ui/tokens/src'
 
 export function App() {
   return <h1 style={{ color: colors.gray900 }}>Hello World!</h1>
