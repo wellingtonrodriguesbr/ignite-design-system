d6da16128fbace85275c5dc76412fad890795b0a
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-02-21 11:04:16 -0300

    feat: create Button component

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,27 @@
-import { colors } from '@ignite-ui/tokens'
+import { ComponentProps } from 'react'
+import { styled } from './styles'
 
-export function App() {
-  return <h1 style={{ color: colors.gray900 }}>Hello World!</h1>
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  borderRadius: '$sm',
+  backgroundColor: '$ignite300',
+  border: 0,
+  color: '$white',
+  fontWeight: '$bold',
+  variants: {
+    size: {
+      small: {
+        padding: '$2 $4',
+      },
+      big: {
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
